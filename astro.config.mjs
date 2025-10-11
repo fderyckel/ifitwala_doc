2bd1115a23cfe22ca8bcc8e0d92d955fc73f04fd
Francois de Ryckel <f.deryckel@gmail.com>
2025-10-11 23:46:00 +0700

    fdsa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind'
 import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
+  base: '/docs',
   output: 'static',
   outDir: './dist',
   srcDir: './src',
