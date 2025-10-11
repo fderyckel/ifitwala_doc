ee4c5058b5957221f8d5e5a5a34f231f6a7b4dd5
Francois de Ryckel <f.deryckel@gmail.com>
2025-10-11 21:31:12 +0700

    gvfds

---
+++ ifitwala_doc/api/__init__.py
@@ -0,0 +1,17 @@
+from .docs import (
+    fetch_all,
+    fetch_one,
+    search_index,
+    get_categories,
+    get_category,
+    get_docs_in_category,
+)
+
+__all__ = [
+    "fetch_all",
+    "fetch_one",
+    "search_index",
+    "get_categories",
+    "get_category",
+    "get_docs_in_category",
+]
