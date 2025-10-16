5cca4e8a1c404505edd853bff36c63355a4b44b1
Francois de Ryckel <f.deryckel@gmail.com>
2025-10-16 19:55:06 +0700

    fdsa

--- ifitwala_doc/ifitwala_doc/doctype/feature_highlight/feature_highlight.py
+++ ifitwala_doc/ifitwala_doc/doctype/feature_highlight/feature_highlight.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, François de Ryckel and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
-
-class FeatureHighlight(Document):
-	pass
+class FeatureHighlights(Document):
+    def validate(self):
+        if not self.items:
+            frappe.throw(_("Add at least one Feature Item."))
+        labels = set()
+        for row in self.items:
+            if not row.label:
+                frappe.throw(_("Each Feature Item requires a Label."))
+            if row.item_order in labels:
+                frappe.throw(_("Duplicate item_order {0}. Use unique order values.").format(row.item_order))
+            labels.add(row.item_order)
